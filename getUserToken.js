f5f84fead0e4eac7fff3991ba6a0e1ef4325010d
Jack Heriz-Smith <jackherizsmith@gmail.com>
2021-09-15 13:51:42 +0100

    remove condition

--- getUserToken.js
+++ getUserToken.js
@@ -7,13 +7,10 @@ module.exports = async (req, res) => {
 
   const streamChatServerClient = new StreamChat(config.STREAM_PUBLIC, config.STREAM_SECRET);
 
-  if (req.method === 'POST') {
-    try {
-      const chatToken = streamChatServerClient.createToken(req.body.id);
-      return res.end(JSON.stringify({chatToken}));
-    } catch (error) {
-      return res.status(400).end(JSON.stringify({error}));
-    }
+  try {
+    const chatToken = streamChatServerClient.createToken(req.body.id);
+    return res.end(JSON.stringify({chatToken}));
+  } catch (error) {
+    return res.status(400).end(JSON.stringify({error: "Bad request"}));
   }
-  return res.status(404).end();
 };
